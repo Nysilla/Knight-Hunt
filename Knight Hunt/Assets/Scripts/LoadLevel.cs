564209787a2784e87bfae1cebc898a2877f740d5
Raf <25.rafael.gutierrez@ksd.org>
2023-10-23 12:34:23 -0700

    created main menu

---
+++ Knight Hunt/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour
+{
+    public void LoadAScene(int scene)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+    }
+}
