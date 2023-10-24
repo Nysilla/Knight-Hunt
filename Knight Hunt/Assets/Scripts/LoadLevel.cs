ea46b809335c2538a1776e789f53c323b6593f00
Raf <25.rafael.gutierrez@ksd.org>
2023-10-24 12:38:42 -0700

    added sound effects

--- Knight Hunt/Assets/Scripts/LoadLevel.cs
+++ Knight Hunt/Assets/Scripts/LoadLevel.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class LoadLevel : MonoBehaviour
 {
+
     public void LoadAScene(int scene)
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+    }
+
+    public void PlaySFX(AudioClip audioClip)
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.PlayOneShot(audioClip);
     }
 
     public void QuitGame()
