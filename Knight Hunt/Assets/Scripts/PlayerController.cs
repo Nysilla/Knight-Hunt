d969f9edca1d0e196460fd695a2f3e1803a2f5b7
Raf <25.rafael.gutierrez@ksd.org>
2023-10-25 14:00:03 -0700

    changes to my sandbox

--- Knight Hunt/Assets/Scripts/PlayerController.cs
+++ Knight Hunt/Assets/Scripts/PlayerController.cs
@@ -2,43 +2,80 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float playerSpeed = 20, jumpHeight = 20; 
-    private float multiplier = 100;
+    [SerializeField] float playerSpeed = 1, jumpHeight = 1, multiplier = 10;
     private Rigidbody2D rigidbody;
-    private Collider2D collider;
+    private CapsuleCollider2D collider;
+    private AudioSource audioSource;
+    bool isMoving;
+    [SerializeField] AudioClip[] audioClips;
+    float audioLength;
+    int audioClip = 0;
+
+    Vector2 moveInput;
     //private KeyCode sprint = KeyCode.LeftShift;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        collider = GetComponent<Collider2D>();
+        collider = GetComponent<CapsuleCollider2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        MovePlayer();
-        PlayerJump();
+        PlayerRun();
     }
 
-    private void PlayerJump()
+    private void PlayerRun()
     {
-        if(collider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        rigidbody.velocity = new Vector2(x: playerSpeed * moveInput.x * multiplier * Time.deltaTime, y: rigidbody.velocity.y);
+
+        Debug.Log(isMoving = Mathf.Abs(rigidbody.velocity.x) > 0);
+
+        if (isMoving
+            && collider.IsTouchingLayers(LayerMask.GetMask("Ground"))
+            && audioLength >= audioClips[audioClip].length
+            && audioClip != audioClips.Length)
         {
-            rigidbody.velocity += new Vector2(x: 0, y: Input.GetAxisRaw("Jump") * jumpHeight * multiplier * Time.deltaTime);
+            PlaySFX();
+        }
+        audioLength += Time.deltaTime;
+    }
+
+    private void PlaySFX()
+    {
+        if (audioClip < audioClips.Length - 1)
+        {
+            audioClip++;
+        }
+        else
+        {
+            audioClip = 0;
+        }
+        audioSource.PlayOneShot(audioClips[audioClip]);
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if(!collider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
         }
 
-        
+        if(value.isPressed)
+        {
+            rigidbody.velocity += new Vector2(x: 0, y: jumpHeight * multiplier * Time.deltaTime);
+        }
     }
-
-    private void MovePlayer()
-    {
-        Vector2 movePlayer = new(x: Input.GetAxisRaw("Horizontal") * playerSpeed * multiplier * Time.deltaTime, y: rigidbody.velocity.y);
-
-        rigidbody.velocity = movePlayer;
-    }
+    
 }
