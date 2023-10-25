05db3a67ecabd437ffbdf9a759235c34a694c480
Raf <25.rafael.gutierrez@ksd.org>
2023-10-25 12:45:24 -0700

    changes to sandbox

--- Knight Hunt/Assets/Scripts/PlayerController.cs
+++ Knight Hunt/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float playerSpeed = 20, jumpHeight = 20;
+    [SerializeField] float playerSpeed = 20, jumpHeight = 20; 
+    private float multiplier = 100;
     private Rigidbody2D rigidbody;
     private Collider2D collider;
-    private KeyCode jump = KeyCode.Space, forward = KeyCode.D, backward = KeyCode.A, sprint = KeyCode.LeftShift;
+    //private KeyCode sprint = KeyCode.LeftShift;
 
     // Start is called before the first frame update
     void Start()
@@ -26,9 +27,9 @@ public class PlayerController : MonoBehaviour
 
     private void PlayerJump()
     {
-        if(Input.GetKeyDown(jump) && collider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        if(collider.IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
-            rigidbody.velocity = new Vector2(x: 0, y: jumpHeight * 10 * Time.deltaTime);
+            rigidbody.velocity += new Vector2(x: 0, y: Input.GetAxisRaw("Fire1") * jumpHeight * multiplier * Time.deltaTime);
         }
 
         
@@ -36,17 +37,8 @@ public class PlayerController : MonoBehaviour
 
     private void MovePlayer()
     {
-        Vector2 movePlayer = new(x: playerSpeed * 10 * Time.deltaTime, y: 0);
+        Vector2 movePlayer = new(x: Input.GetAxisRaw("Horizontal") * playerSpeed * multiplier * Time.deltaTime, y: rigidbody.velocity.y);
 
-        if (Input.GetKey(forward))
-        {
-            rigidbody.velocity = Input.GetKey(sprint) ? movePlayer * 2 : movePlayer;
-
-        }
-        else if (Input.GetKey(backward))
-        {
-            rigidbody.velocity = Input.GetKey(sprint) ? -(movePlayer * 2) : -movePlayer;
-
-        }
+        rigidbody.velocity = movePlayer;
     }
 }
