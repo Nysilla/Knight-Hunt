05381f3e8c0e50601e7fb15a3153dac2faa043e5
Seraph-21 <147756663+Seraph-21@users.noreply.github.com>
2023-10-27 12:00:41 -0700

    Add files via upload

---
+++ Knight Hunt/Assets/Scripts/Projectile.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Rigidbody2D projectileRb;
+    public float speed;
+    public float projectileLife;
+    public float projectileCount;
+    public PlayerMovement playerMovement;
+    public bool facingright;
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectileCount = projectileLife;
+        playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        facingright = playerMovement.playerHasHorizontalSpeed;
+        if (!facingright)
+        { 
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else 
+        {
+            transform.rotation = Quaternion.Euler(0, 0, -1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        projectileCount -= Time.deltaTime;
+        if(projectileCount <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void FixedUpdate()
+    {
+        if(!facingright ==true)
+        {
+            
+            projectileRb.velocity = new Vector2(+speed, projectileRb.velocity.y);
+        }
+        else
+        {
+           
+            projectileRb.velocity = new Vector2(-speed, projectileRb.velocity.y);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            collision.gameObject.GetComponent<EnemyHealth>().TakeDamage(1);
+        }
+        Destroy(gameObject);
+    }
+}
