10b9390f11a21ed34c32dd2eada7be3c70554494
Raf <25.rafael.gutierrez@ksd.org>
2023-10-27 13:25:49 -0700

    added player damage

---
+++ Knight Hunt/Assets/Scripts/PlayerDamage.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour
+{
+    [SerializeField] float distance = 3, damageDelay = 1;
+    [SerializeField] int damageAmount;
+    float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        Vector3 enemyPosition = GameObject.FindWithTag("Enemy").transform.position - transform.position;
+
+        if(enemyPosition.magnitude < distance && Input.GetKeyDown(KeyCode.Mouse0) && timer >= damageDelay)
+        {
+            GameObject.FindWithTag("Enemy").GetComponent<EnemyHealth>().TakeDamage(damageAmount);
+            timer = 0;
+        }
+    }
+}
