61f7e710dd9b4964578de91ef4d811125eed6dab
Raf <25.rafael.gutierrez@ksd.org>
2023-11-09 12:49:43 -0800

    changes to scripts

--- Knight Hunt/Assets/Scripts/EnemyMovement.cs
+++ Knight Hunt/Assets/Scripts/EnemyMovement.cs
@@ -9,37 +9,35 @@ public class EnemyMovement : MonoBehaviour
     //Internal Variables
     private Rigidbody2D rb;
     private GameObject player;
-    private bool isMoving;
+    public bool canMove = true;
+    public Mathf mathF;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player");
     }
 
     void FixedUpdate()
     {
-        isMoving = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
-        player = GameObject.FindWithTag("Player");
+        //player = GameObject.FindWithTag("Player");
 
-        MoveEnemy();
-        FlipEnemyFacing();
+        if (canMove)
+        {
+            MoveEnemy();
+            FlipEnemyFacing();
+        }
+        
     }
 
     private void MoveEnemy()
     {
         Vector3 playerPosition = player.transform.position - transform.position;
         bool playerIsClose = playerPosition.magnitude < chaseDistance;
-        
-        if (playerIsClose)
-        {
-            playerPosition.Normalize();
-            Vector2 moveEnemy = playerPosition * new Vector2(x: 100 * Time.deltaTime * moveSpeed, y: rb.velocity.y);
-            rb.velocity = moveEnemy;
-        }
-        else
-        {
-            rb.velocity = Vector2.zero;
-        }
+
+        playerPosition.Normalize();
+        Vector2 moveEnemy = playerPosition.x * new Vector2(x: 100 * Time.deltaTime * moveSpeed, y: rb.velocity.y);
+        rb.velocity = playerIsClose ? moveEnemy : Vector3.zero;
     }
 
     void FlipEnemyFacing()
