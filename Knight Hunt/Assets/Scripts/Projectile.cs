247ccc800f6cca2c3c8ccd6f5d2e5b636864bd1f
Seraph-21 <147756663+Seraph-21@users.noreply.github.com>
2023-11-09 12:51:52 -0800

    Updated Projectile.cs

--- Knight Hunt/Assets/Scripts/Projectile.cs
+++ Knight Hunt/Assets/Scripts/Projectile.cs
@@ -1,59 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Projectile : MonoBehaviour
 {
     public Rigidbody2D projectileRb;
+
     public float speed;
     public float projectileLife;
     public float projectileCount;
-    public PlayerMovement playerMovement;
-    public bool facingright;
+
+    [SerializeField] int dmg;
+
     // Start is called before the first frame update
     void Start()
     {
         projectileCount = projectileLife;
-        playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
-        //facingright = playerMovement.playerHasHorizontalSpeed;
-        facingright = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().velocity.x > Mathf.Epsilon;
-        if (!facingright)
-        { 
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-        }
-        else 
-        {
-            transform.rotation = Quaternion.Euler(0, 0, -1);
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
         projectileCount -= Time.deltaTime;
-        if(projectileCount <= 0)
+
+        if (projectileCount <= 1)
         {
             Destroy(gameObject);
         }
     }
+    
     private void FixedUpdate()
     {
-        if(!facingright ==true)
+        if (Input.GetKey(KeyCode.Q))
         {
-            
-            projectileRb.velocity = new Vector2(+speed, projectileRb.velocity.y);
-        }
-        else
-        {
-           
             projectileRb.velocity = new Vector2(-speed, projectileRb.velocity.y);
+            transform.rotation = Quaternion.Euler(0, -180, 0);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            projectileRb.velocity = new Vector2(+speed, projectileRb.velocity.y);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
+    
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "enemy")
         {
-            collision.gameObject.GetComponent<EnemyHealth>().TakeDamage(1);
+            collision.gameObject.GetComponent<EnemyHealth>().TakeDamage(dmg);
         }
         Destroy(gameObject);
     }
