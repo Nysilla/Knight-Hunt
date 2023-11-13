f1b8928fd97da261b41c2b29deb37d3696e41006
Raf <25.rafael.gutierrez@ksd.org>
2023-11-13 14:00:21 -0800

    changes to scripts

--- Knight Hunt/Assets/Scripts/EnemyHealth.cs
+++ Knight Hunt/Assets/Scripts/EnemyHealth.cs
@@ -27,14 +27,14 @@ public class EnemyHealth : MonoBehaviour
     {
         Vector3 enemyFacing = gameObject.transform.localScale.x < 0 ? new(-textScale.x, textScale.y, textScale.z) : textScale;
         healthText.transform.localScale = enemyFacing;
-        string enemyNameAndHealth = $"Health: {currentHealth}";
+        string enemyHealthText = $"Health: {currentHealth}";
 
         if (currentHealth < 100)
         {
             healthText.enabled = true;
         }
 
-        healthText.text = enemyNameAndHealth;
+        healthText.text = enemyHealthText;
     }
 
     public void TakeDamage(int amount)
--- Knight Hunt/Assets/Scripts/Respawn.cs
+++ Knight Hunt/Assets/Scripts/Respawn.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class Respawn : MonoBehaviour
 {
+    /* [SerializeField] private bool respawnPlayer;
+    [SerializeField] private int damageAmount; */
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -12,4 +15,12 @@ public class Respawn : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
+
+    /* private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !respawnPlayer)
+        {
+            GameObject.FindWithTag("Player").GetComponent<PlayerHealth>().TakeDamage(damageAmount);
+        }
+    } */
 }
